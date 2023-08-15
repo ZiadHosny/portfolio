8add83491cc4315e3206a356ebe75939cfc2296d
ziad <ziad.hosny@bitspire.ca>
2023-08-16 02:18:07 +0300

    add skills page

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { motion } from "framer-motion";
+import { SkillsCard } from "@/components/skillsCard/SkillsCard"
+import "./Skills.css";
+
+import {
+    SkillType,
+    backendData,
+    frontEndData,
+    programmingLanguagesData,
+    toolsData,
+} from "@/lib/data";
+
+
+const SkillsSection = ({ heading, data }: { heading: string, data: SkillType[] }) => {
+    return (
+        <div className="skills-card-container">
+            <h2 className="category-heading">{heading}</h2>
+            <div className="skill">
+                {data.map((props, index) => (
+                    <SkillsCard {...props} index={index} key={crypto.randomUUID()} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default function Skills() {
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: -200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+            className="skills-container"
+        >
+            <h1 className="heading">My skills tree</h1>
+            <div className="all-skills-card-container">
+                <SkillsSection heading="Programming languages" data={programmingLanguagesData} />
+                <SkillsSection heading="Front-end" data={frontEndData} />
+                <SkillsSection heading="back-end" data={backendData} />
+                <SkillsSection heading="Other tools" data={toolsData} />
+            </div>
+        </motion.div>
+    );
+};
---
+++ src/lib/data.tsx
@@ -0,0 +1,112 @@
+import {
+    SiPostman,
+    SiGitlab,
+    SiAngular,
+    SiBootstrap,
+    SiCanva,
+    SiChakraui,
+    SiMongodb,
+    SiMongoose,
+    SiCss3,
+    SiNodedotjs,
+    SiFigma,
+    SiGit,
+    SiGithub,
+    SiHtml5,
+    SiGatsby,
+    SiBackstage,
+    SiJavascript,
+    SiMui,
+    SiNotion,
+    SiNpm,
+    SiExpress,
+    SiPython,
+    SiDocker,
+    SiReact,
+    SiJsonwebtokens,
+    SiPostgresql,
+    SiSass,
+    SiStyledcomponents,
+    SiTailwindcss,
+    SiTypescript,
+} from "react-icons/si";
+
+import { TbBrandNextjs } from 'react-icons/tb'
+
+import { GrMysql } from "react-icons/gr";
+
+import { ReactNode } from "react";
+
+export type SkillType = {
+    icon: ReactNode;
+    name: string;
+    mainColor: string;
+    ligthColor: string;
+}
+
+const accentColors = {
+    mainColor: "var(--accent-ligth)",
+    ligthColor: "var(--accent)",
+}
+
+const lightColors = {
+    mainColor: "var(--ligth-main)",
+    ligthColor: "var(--clear-ligth)",
+}
+
+const createIcon = ({ icon, name, id }: { icon: ReactNode, name: string, id: number }): SkillType => {
+    const colors = id % 2 === 0 ? accentColors : lightColors
+    return {
+        icon,
+        name,
+        ...colors
+    }
+}
+
+export const programmingLanguagesData: SkillType[] = [
+    createIcon({ id: 1, icon: <SiJavascript />, name: "JavaScript" }),
+    createIcon({ id: 2, icon: <SiTypescript />, name: "TypeScript" }),
+    createIcon({ id: 3, icon: <SiPython />, name: "Python" }),
+];
+
+export const frontEndData: SkillType[] = [
+    createIcon({ id: 1, icon: <SiReact />, name: "React" }),
+    createIcon({ id: 2, icon: <TbBrandNextjs />, name: "NextJs" }),
+    createIcon({ id: 3, icon: <SiAngular />, name: "Angular" }),
+    createIcon({ id: 4, icon: <SiHtml5 />, name: "HTML" }),
+    createIcon({ id: 3, icon: <SiCss3 />, name: "CSS" }),
+    createIcon({ id: 4, icon: <SiMui />, name: "MaterialUI" }),
+    createIcon({ id: 3, icon: <SiChakraui />, name: "Chakra UI" }),
+    createIcon({ id: 4, icon: <SiStyledcomponents />, name: "StyledComponents" }),
+    createIcon({ id: 3, icon: <SiBootstrap />, name: "Bootstrap" }),
+    createIcon({ id: 4, icon: <SiSass />, name: "Sass" }),
+    createIcon({ id: 5, icon: <SiTailwindcss />, name: "Tailwind" }),
+    createIcon({ id: 6, icon: <SiGatsby />, name: "Gatsby" }),
+    createIcon({ id: 7, icon: <SiBackstage />, name: "BackstageJS" }),
+];
+
+export const backendData: SkillType[] = [
+    createIcon({ id: 1, icon: <SiNodedotjs />, name: "NodeJs" }),
+    createIcon({ id: 2, icon: <SiExpress />, name: "Express" }),
+    createIcon({ id: 3, icon: <SiMongodb />, name: "Mongodb" }),
+    createIcon({ id: 4, icon: <SiMongoose />, name: "Mongoose" }),
+    createIcon({ id: 5, icon: <SiPostgresql />, name: "Postgresql" }),
+    createIcon({ id: 6, icon: <GrMysql />, name: "Mysql" }),
+    createIcon({ id: 7, icon: <SiDocker />, name: "Docker" }),
+    createIcon({ id: 8, icon: <SiJsonwebtokens />, name: "JWT" }),
+
+];
+
+export const toolsData: SkillType[] = [
+    createIcon({ id: 8, icon: <SiPostman />, name: "Postman" }),
+    createIcon({ id: 1, icon: <SiGit />, name: "Git" }),
+    createIcon({ id: 2, icon: <SiGithub />, name: "GitHub" }),
+    createIcon({ id: 3, icon: <SiGitlab />, name: "Gitlab" }),
+    createIcon({ id: 4, icon: <SiNpm />, name: "NPM" }),
+    createIcon({ id: 5, icon: <SiFigma />, name: "Figma" }),
+    createIcon({ id: 6, icon: <SiCanva />, name: "Canva" }),
+    createIcon({ id: 7, icon: <SiNotion />, name: "Notion" }),
+
+];
+
+
