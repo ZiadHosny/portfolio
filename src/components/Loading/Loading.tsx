7240e3d69a4ac5cac6c3875561973e070f6d8e45
ziad <ziad.hosny@bitspire.ca>
2023-08-17 01:12:22 +0300

    add loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import './Loading.css'
+
+export const Loading = () => {
+    return (
+        <div className="center-body" >
+            <div className="loader-circle-9" > Loading
+                <span></span >
+            </div >
+        </div >
+
+    )
+}
