a365f878a9455992f25a3d4735ecffbb84e23c57
ziad <ziad.hosny@bitspire.ca>
2023-09-23 14:08:44 +0300

    add certification

--- src/lib/getCertifications.ts
+++ src/lib/getCertifications.ts
@@ -18,7 +18,7 @@ export const certifications: Certification[] = [
         place: "Mansoura University",
         date: "07/2019 - 10/2019",
         certificateAvailable: true,
-        certificateImage: "./certification/Consulting of Computers and Information Center - Java & Android.png",
+        certificateImage: "v1695465690/Certifications/Consulting_of_Computers_and_Information_Center_-_Java_Android_sfcwa1.png",
     },
     {
         title: "Flutter",
@@ -26,7 +26,7 @@ export const certifications: Certification[] = [
         place: "Mega Trust Company",
         date: "02/2022 - 03/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/MegaTrust-Flutter.JPG",
+        certificateImage: "v1695465693/Certifications/MegaTrust-Flutter_xevlcw.jpg",
     },
     {
         title: "HTML & CSS",
@@ -34,7 +34,7 @@ export const certifications: Certification[] = [
         place: "ITI",
         date: "05/2022 - 06/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/Mahara Tech -  HTML & CSS.png",
+        certificateImage: "v1695462263/Certifications/MaharaTech/Mahara_Tech_-_HTML_CSS_f4rchc.png",
         certificateLink: "https://maharatech.gov.eg/mod/customcert/view.php?id=1404&downloadown=1"
     },
     {
@@ -43,7 +43,7 @@ export const certifications: Certification[] = [
         place: "ITI",
         date: "05/2022 - 06/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/Mahara Tech - Database.png",
+        certificateImage: "v1695462273/Certifications/MaharaTech/Mahara_Tech_-_Database_dww26y.png",
         certificateLink: "https://maharatech.gov.eg/mod/customcert/view.php?id=7655&downloadown=1"
     },
     {
@@ -52,7 +52,7 @@ export const certifications: Certification[] = [
         place: "ITI",
         date: "05/2022 - 06/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/Mahara Tech - Javascript.png",
+        certificateImage: "v1695462271/Certifications/MaharaTech/Mahara_Tech_-_Javascript_jxbvaw.png",
         certificateLink: "https://maharatech.gov.eg/mod/customcert/view.php?id=7660&downloadown=1"
     },
     {
@@ -61,7 +61,7 @@ export const certifications: Certification[] = [
         place: "ITI",
         date: "06/2022 - 07/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/Mahara Tech - Typescript.png",
+        certificateImage: "v1695462278/Certifications/MaharaTech/Mahara_Tech_-_Typescript_uw11g6.png",
         certificateLink: "https://maharatech.gov.eg/mod/customcert/view.php?id=8663&downloadown=1"
     },
     {
@@ -70,7 +70,7 @@ export const certifications: Certification[] = [
         place: "Information Technology Institute",
         date: "04/2022 - 08/2022",
         certificateAvailable: true,
-        certificateImage: "./certification/ITI.JPG",
+        certificateImage: `v1695465692/Certifications/ITI_zq9prq.jpg`,
     },
     {
         title: "JavaScript (Basic)",
@@ -78,7 +78,7 @@ export const certifications: Certification[] = [
         place: "Online",
         date: "08/2022",
         certificateAvailable: true,
-        certificateImage: `${cloudinary}/v1695462880/Certifications/HackerRank/javascript_basic_certificate_qpffng.png`,
+        certificateImage: `v1695462880/Certifications/HackerRank/javascript_basic_certificate_qpffng.png`,
         certificateLink: "https://www.hackerrank.com/certificates/150b23da9a18"
     },
     {
@@ -87,7 +87,7 @@ export const certifications: Certification[] = [
         place: "Online",
         date: "08/2022",
         certificateAvailable: true,
-        certificateImage: `${cloudinary}/v1695462963/Certifications/HackerRank/react_basic_certificate_akykwl.png`,
+        certificateImage: `v1695462963/Certifications/HackerRank/react_basic_certificate_akykwl.png`,
         certificateLink: "https://www.hackerrank.com/certificates/240ca2891d46"
     },
     {
@@ -96,7 +96,7 @@ export const certifications: Certification[] = [
         place: "Online",
         date: "08/2022",
         certificateAvailable: true,
-        certificateImage: `${cloudinary}/v1695462873/Certifications/HackerRank/problem_solving_basic_certificate_nggpyh.png`,
+        certificateImage: `v1695462873/Certifications/HackerRank/problem_solving_basic_certificate_nggpyh.png`,
         certificateLink: "https://www.hackerrank.com/certificates/a3fb40835128"
     },
     {
@@ -105,8 +105,26 @@ export const certifications: Certification[] = [
         place: "Online",
         date: "09/2022",
         certificateAvailable: true,
-        certificateImage: `${cloudinary}/v1695462867/Certifications/HackerRank/javascript_intermediate_certificate_hyduus.png`,
+        certificateImage: `v1695462867/Certifications/HackerRank/javascript_intermediate_certificate_hyduus.png`,
         certificateLink: "https://www.hackerrank.com/certificates/a8db3bcdddae"
+    },
+    {
+        title: "Responsive Web Design",
+        courseType: "Free Code Camp",
+        place: "Online",
+        date: "10/2022",
+        certificateAvailable: true,
+        certificateImage: `v1695466627/Certifications/FreeCodeCamp/responsive-web-design_yaew80.png`,
+        certificateLink: "https://www.freecodecamp.org/certification/ziadhosny/responsive-web-design"
+    },
+    {
+        title: "JavaScript Algorithms And Data Structures",
+        courseType: "Free Code Camp",
+        place: "Online",
+        date: "10/2022",
+        certificateAvailable: true,
+        certificateImage: `v1695466627/Certifications/FreeCodeCamp/javascript-algorithms-and-data-structures_k17kon.png`,
+        certificateLink: "https://www.freecodecamp.org/certification/ziadhosny/javascript-algorithms-and-data-structures"
     },
     {
         title: "NodeJs (Intermediate)",
@@ -114,7 +132,7 @@ export const certifications: Certification[] = [
         place: "Online",
         date: "09/2023",
         certificateAvailable: true,
-        certificateImage: `${cloudinary}/v1695462878/Certifications/HackerRank/nodejs_intermediate_certificate_qypwwn.png`,
+        certificateImage: `v1695462878/Certifications/HackerRank/nodejs_intermediate_certificate_qypwwn.png`,
         certificateLink: "https://www.hackerrank.com/certificates/e87ef0597dd9"
     },
-].reverse();
+].reverse().map((e) => ({ ...e, certificateImage: `${cloudinary}/${e.certificateImage}` }));
